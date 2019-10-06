edfe228281b3cf55f6ed18a05482f77487f70654
WindMark <44115005+spanishinquistion@users.noreply.github.com>
2019-10-06 11:51:42 +0200

    Add files via upload

--- powerCurve.js
+++ powerCurve.js
@@ -5,6 +5,7 @@ var c2 = 'Red'
 var c3 = 'royalblue' 
 var c4 = 'Cyan' 
 var pc = [0.0, 0.0, 0.0, 2.262406473988439, 4.4187626445086705, 7.635621849710983, 12.125084696531793, 18.099251791907513, 25.77022374277457, 35.350101156069364, 47.05098463872832, 61.08497479768786, 77.6641722398844, 97.00067757225435, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0]
+var font_1 = 18
 // Data Above Template Below
 var ctx = document.getElementById(chart_name).getContext('2d');
 
@@ -38,20 +39,24 @@ var myChart = new Chart(ctx, {
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true
+          beginAtZero: true,
+          fontSize: font_1
         },
         scaleLabel:{
           display: true,
-          labelString: 'Power Production [%/rated]'
+          labelString: 'Power Production [%/rated]',
+          fontSize: font_1
         }
       }],
       xAxes: [{
         ticks:{
-          beginAtZero: true
+          beginAtZero: true,
+          fontSize: font_1
         },
         scaleLabel: {
           display: true,
-          labelString: 'Wind Speed [m/s]'
+          labelString: 'Wind Speed [m/s]',
+          fontSize: font_1
         }
       }]
     }
